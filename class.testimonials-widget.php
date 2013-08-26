8447a1f3f84b04105eb619b5c571b1115add5cc8
Luke Rollans <hello@lukerollans.me>
2013-08-26 17:50:31 +0800

    Coded Testimonial Widget

--- class.testimonials-widget.php
+++ class.testimonials-widget.php
@@ -16,14 +16,37 @@ final class Testimonials_Widget extends WP_Widget {
 	}
 	
 	function update ( $new_instance, $old_instance ) {
-	
 		
+		$instance = array( 'testimonial_id' => 'random' );
+		
+		if( !empty( $new_instance['testimonial_id'] ) )
+			$instance['testimonial_id'] = $new_instance['testimonial_id'];
+			
+		return $instance;
 	
 	}
 	
 	function form ( $instance ) {
-	
+
+		?>
 		
+		<p>Select a Testimonial to display</p>
+		
+		<select name="testimonial_id" style="width:100%;">
+		
+			<option value="random">Random</option>	
+		
+			<?php if( $testimonials = get_posts( array( 'post_type' => 'testimonial', 'numberposts' => -1 ) ) ) :?>
+			
+			<?php foreach( $testimonials as $testimonial ): ?>
+			<option value="<?php echo $testimonial->ID; ?>"<?php echo ( $instance['testimonial_id'] == $testimonial->ID ? ' selected="selected"' : NULL ); ?>><?php echo $testimonial->post_title; ?></option>
+			<?php endforeach; ?>
+			
+			<?php endif; ?>
+			
+		</select>
+		
+		<?php
 	
 	}
 	
