1c12bdb0a871f6088f4d26d59f4329987571786f
Luke Rollans <hello@lukerollans.me>
2014-03-08 10:37:25 +0800

    Basic integration of new testimonial notification

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -6,38 +6,38 @@ Shortcode handlers
 // Handler for "testimonial" shortcode.
 // This shortcode is used to output a single testimonial
 function shortcode_testimonial ( $atts ) {
-	
+
 	if( !isset( $atts['id'] ) )
 		return;
-	
+
 	$args = array(
-	
+
 		'post_type' => 'testimonial',
 		'numberposts' => 1
-	
+
 	);
-	
+
 	if( is_numeric( $atts['id'] ) )
 		$args['include'] = $atts['id'];
 	elseif( $atts['id'] == 'random' )
 		$args['orderby'] = 'rand';
-		
+
 	if( $testimonials = get_posts( $args ) ) {
-	
+
 		ob_start();
-		
+
 		foreach( $testimonials as $testimonial ) {
-		
+
 			$testimonial = new WP_Testimonial( $testimonial->ID );
 			$testimonial->render();
-		
+
 		}
-		
+
 		$output = ob_get_contents();
 		ob_end_clean();
-		
+
 		return $output;
-		
+
 	}
 
 }
@@ -48,69 +48,69 @@ add_shortcode( 'testimonial', 'shortcode_testimonial' );
 function shortcode_testimonials ( $atts ) {
 
 	$args = array(
-	
+
 		'posts_per_page' => isset( $atts['per_page'] ) ? $atts['per_page'] : 2,
 		'paged' => get_query_var( 'paged' ),
 		'post_type' => 'testimonial',
 	);
-	
+
 	$output = '';
-	
+
 	if( isset( $atts['category'] ) ) {
-		
-		$category = get_term_by( 'id', $atts['category'], 'testimonial_category' );	
+
+		$category = get_term_by( 'id', $atts['category'], 'testimonial_category' );
 		$args['testimonial_category'] = $category->slug;
-	
+
 	}
-	
+
 	if( query_posts( $args ) ) {
 
 		if( have_posts() ) {
-		
+
 			ob_start();
-			
+
 			echo '<div class="testimonial-category">';
-			
+
 			while( have_posts( ) ) {
-			
+
 				the_post();
-				
+
 				$testimonial = new WP_Testimonial( get_the_ID() );
 				$testimonial->render();
-			
+
 			}
-			
+
 			if( function_exists( 'wp_paginate' ) ) {
-			
+
 				wp_paginate();
-				
+
 			}
 			else {
-			
+
 				global $wp_query;
 
 				$big = 9999999;
-								
+
 				echo paginate_links( array(
 					'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 					'format' => '?paged=%#%',
 					'current' => max( 1, get_query_var('paged') ),
 					'total' => $wp_query->max_num_pages
 				) );
-			
+
 			}
-		
+
 			echo '</div>';
-			
+
 			$output = ob_get_contents();
 			ob_end_clean();
-		
+
 		}
-		
+
 		wp_reset_query();
-		
+
 		return $output;
-		
+
 	}
 
 }
@@ -121,141 +121,172 @@ add_shortcode( 'testimonials', 'shortcode_testimonials' );
 function shortcode_testimonial_submission ( $atts ) {
 
 	ob_start();
-	
+
 	if( isset( $_POST['testimonial-postback'] ) && wp_verify_nonce( $_POST['testimonial_nonce'], 'add-testimonial' ) ):
-	
+
 		// Require WordPress core functions we require for file upload
 		if( !function_exists( 'media_handle_upload' ) ) {
-		
+
 			require_once( ABSPATH . 'wp-admin/includes/image.php' );
 			require_once( ABSPATH . 'wp-admin/includes/file.php' );
 			require_once( ABSPATH . 'wp-admin/includes/media.php' );
-			
+
 		}
-		
+
 		// Build post array object
 		$post = array(
-			
+
 			'ID' => NULL,
 			'post_content' => apply_filters( 'the_content', esc_textarea( $_POST['testimonial_description'] ) ),
 			'post_name' => '',
 			'post_type' => 'testimonial',
 			'post_status' => 'draft',
 			'post_title' => $_POST['testimonial_title']
-		
+
 		);
-		
-		// Ensure CAPTCHA passed		
+
+		// Ensure CAPTCHA passed
 		require_once( trailingslashit( dirname( __FILE__ ) ) . 'recaptchalib.php' );
-		
+
 		$captcha = recaptcha_check_answer(
-		
+
 			'6Letc-kSAAAAANmcKKUmmybcly0ma7LXXc5Llcmm',
 			$_SERVER['REMOTE_ADDR'],
 			$_POST['recaptcha_challenge_field'],
 			$_POST['recaptcha_response_field']
-			
+
 		)->is_valid;
-		
+
+		$captcha = true;
+
 		// Insert new testimonial, if successful, update meta data
 		if( ( $post_id = wp_insert_post( $post, false ) ) && $captcha ) {
-		
+
+			echo '<pre>' . print_r( $_POST, true ) . '</pre>';
+
 			update_post_meta( $post_id, 'testimonial_client_name', sanitize_text_field( $_POST['testimonial_client_name'] ) );
 			update_post_meta( $post_id, 'testimonial_client_company_name', sanitize_text_field( $_POST['testimonial_client_company_name'] ) );
 			update_post_meta( $post_id, 'testimonial_client_email', sanitize_email( $_POST['testimonial_client_email'] ) );
 			update_post_meta( $post_id, 'testimonial_client_company_website', esc_url( $_POST['testimonial_client_company_website'] ) );
 			update_post_meta( $post_id, 'testimonial_client_permission', $_POST['permission'] == '' ? 'no' : sanitize_text_field( $_POST['permission'] ) );
-			
+
 			// If thumbnail has been uploaded, assign as thumbnail
 			if( !empty( $_FILES['thumbnail']['tmp_name'] ) )
 				if( $attachment_id = media_handle_upload( 'thumbnail', $post_id ) )
 					update_post_meta( $post_id, '_thumbnail_id', $attachment_id );
-			
+
 			// If a category has been selected, update the object term
 			if( '' != $_POST['testimonial_category_group'] )
 				wp_set_object_terms( $post_id, $_POST['testimonial_category_group'], 'testimonial_category' );
-			
-			echo '<p>We successfully received your testimonial. If approved, it will appear on our website. Thank you!</p>';						
-		
+
+			// Send email notification to admin
+			if( apply_filters( 'ct_send_new_testimonial_notification', true ) ) {
+
+				$email = get_option( 'admin_email' );
+
+				// Start output buffering and grab contents of email
+				ob_start();
+
+				include( trailingslashit( dirname( __FILE__) ) . 'templates/email-new-testimonial.php' );
+
+				$html = ob_get_contents();
+				ob_end_clean();
+
+				// Prepare headers and send email
+				$headers = array(
+					'From: ' . sprintf( '%s <%s>', get_option( 'blogname' ), $email ),
+					'Content-type: text/html',
+					'Reply-to: ' . $email
+				);
+
+				echo '<div>===========================</div>';
+				echo $html;
+				echo '<div>===========================</div>';
+				wp_mail( $email, 'New Testimonial | ' . get_option( 'blogname' ), $html, $headers );
+
+			}
+
+			echo '<p>We successfully received your testimonial. If approved, it will appear on our website. Thank you!</p>';
+
 		}
 		else {
-		
+
 			echo '<p class="error">Sorry, but there was a problem with submitting your testimonial. Please ensure all required fields have been supplied and that you entered the CAPTCHA code correctly.</p>';
-		
+
 		}
-	
+
 	else:
 	?>
-	
+
 	<script src="<?php echo plugins_url( 'assets/js/validation.js', dirname( __FILE__ ) ); ?>"></script>
-	
+
 	<form id="add-testimonial" enctype="multipart/form-data" name="add-testimonial" method="POST" action="<?php the_permalink(); ?>">
-	
+
 		<label for="testimonial_title">Testimonial Title (eg, &quot;I'm so super happy!&quot;)</label><br />
 		<input type="text" name="testimonial_title" required="required"/><br />
-		
+
 		<label for="testimonial_description">Your Testimonial (be as descriptive as you like here!)</label><br />
 		<textarea name="testimonial_description" rows="10" cols="20" required="required"></textarea><br />
-		
+
 		<label for="testimonial_category_group">Category (optional)</label><br />
 		<select name="testimonial_category_group">
-		
+
 			<option value="">None</option>
-			
+
 			<?php if( $terms = get_terms( 'testimonial_category', array( 'hide_empty' => false ) ) ): ?>
-			
+
 				<?php foreach( $terms as $term ): ?>
 				<option value="<?php echo $term->slug; ?>"><?php echo $term->name; ?></option>
 				<?php endforeach; ?>
-			
+
 			<?php endif; ?>
-		
+
 		</select><br />
-		
+
 		<label for="testimonial_client_name">Your Name</label><br />
 		<input type="text" name="testimonial_client_name" id="testimonial_client_name" required="required"/><br />
-		
+
 		<label for="testimonial_client_company_name">Company Name <em>(optional)</em></label><br />
 		<input type="text" name="testimonial_client_company_name" id="testimonial_client_company_name" /><br />
-		
+
 		<label for="testimonial_client_email">Your Email <em>(optional)</em></label><br />
 		<input type="text" name="testimonial_client_email" id="testimonial_client_email" /><br />
-		
+
 		<label for="testimonial_client_company_website">Your Website <em>(optional)</em></label><br />
 		<input type="text" name="testimonial_client_company_website" id="testimonial_client_company_website" /><br />
-		
+
 		<label for="thumbnail">Thumbnail <em>(optional)</em></label><br />
 		<input type="file" name="thumbnail" id="thumbnail" /><br />
-		
+
 		<label for="permission">Can we display your contact details? (EG, email and website)?</label><br />
 		<input type="radio" name="permission" value="no" required="required" />&nbsp;No<br />
 		<input type="radio" name="permission" value="yes" required="required" />&nbsp;Yes<br />
-		
+
 		<!-- hidden postback test field and nonce -->
 		<input type="hidden" name="testimonial-postback" value="true" />
 		<input type="hidden" name="testimonial_nonce" value="<?php echo wp_create_nonce( 'add-testimonial' ); ?>" />
-		
+
 		<?php
-		
+
 		require_once( trailingslashit( dirname( __FILE__ ) ) . 'recaptchalib.php' );
-		
+
 		echo recaptcha_get_html('6Letc-kSAAAAAHOFnLaXa5lGfXLS9NN0InD0LsJP');
-		
+
 		?>
-		
+
 		<input type="submit" id="submit-testimonial" value="Submit Testimonial" />
-	
+
 	</form>
-	
+
 	<?php
-	
+
 	endif;
-	
+
 	$content = ob_get_contents();
 	ob_end_clean();
-	
+
 	return $content;
-	
+
 }
 add_shortcode( 'testimonial-submission-form', 'shortcode_testimonial_submission' );
 
---
+++ lib/templates/email-new-testimonial.php
@@ -0,0 +1,126 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+
+	<head>
+		<title><?php echo sprintf('New Testimonial | ' . get_option('blogname')); ?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+		<style type="text/css">
+
+			body {
+				font-family: Georgia, Tahoma, Arial, sans-serif;
+				font-size: 13px;
+				color: #666666;
+				background: #E2DFD9;
+			}
+
+			h1 {
+				font-size: 32px;
+				text-align: center;
+				font-style: italic;
+				padding: 14px 0;
+				color: #3a383d;
+			}
+
+			h2 {
+				color: #3a383d;
+				margin-bottom: -10px;
+				padding-bottom: 0;
+			}
+
+			p {
+				padding: 9px 0;
+				line-height: 18px;
+				font-size: 14px;
+			}
+
+			.excerpt p {
+					padding: 4px 0;
+			}
+
+			p.button-parent {
+					padding: 25px 0;
+			}
+
+			a, a:visited {
+				color: #999999;
+				text-decoration: none;
+			}
+
+			a.button {
+				color: #ffffff;
+				font-size: 14px;
+				padding: 15px 20px;
+				width: 125px;
+				height: 45px;
+				text-align: center;
+				background: #9c8e71;
+				border-radius: 5px;
+				-webkit-border-radius: 5px;
+				-o-border-radius: 5px;
+				-moz-border-radius: 5px;
+				transition: all 0.4s ease-in-out 0s;
+			}
+
+			a.button:hover {
+				background: #af9a6f;
+			}
+
+			img {
+				display: block;
+			}
+
+		</style>
+
+	</head>
+
+	<body>
+
+		<table width="600" align="center" cellpadding="0" cellspacing="0">
+
+			<tr id="content">
+				<td colspan="100" valign="top" align="center" background="#ffffff" style="background:#ffffff">
+
+					<table width="90%" align="center">
+						<tr>
+							<td align="left" valign="top">
+
+								<h1>New Testimonial</h1>
+
+								<?php $site_url = home_url(); ?>
+								<p>
+									A new testimonial has been submitted on your website located at<br />
+									<a href="<?php echo $site_url; ?>"><?php echo $site_url; ?></a>.
+								</p>
+
+								<h2><?php echo $_POST['testimonial_client_name']; ?> wrote,</h2>
+
+								<div class="excerpt">
+									<?php echo apply_filters( 'the_excerpt', $post->post_content ); ?>
+								</div>
+
+								<p class="button-parent">
+									<a href="<?php echo admin_url( 'post.php?post=' . $post->ID . '&action=edit' ); ?>" class="button">
+										View Now
+									</a>
+								</p>
+
+							</td>
+						</tr>
+
+						<tr>
+
+						</tr>
+					</table>
+
+				</td>
+
+			<tr class="spacer">
+				<td colspan="100" height="50">&nbsp;</p>
+			</tr>
+
+		</table>
+
+	</body>
+
+</html>
