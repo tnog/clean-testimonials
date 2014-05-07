22df2f5951a3dbba2d42f21823d52fe1abe0cb23
Takahiro Noguchi <takahiro.noguchi@gmail.com>
2014-05-06 17:03:25 -0700

    Added class to span tag

--- class.wp-testimonial.php
+++ class.wp-testimonial.php
@@ -40,7 +40,7 @@ final class WP_Testimonial {
 
 			<blockquote>
 				<p>
-					<span>
+					<span class="quote-container">
 
 						<?php if( has_post_thumbnail( $this->ID ) ): $image = wp_get_attachment_image_src( get_post_thumbnail_id( $this->ID ), array( 200, 200 ) ); ?>
 						<img style="float: left; padding: 10px;" src="<?php echo $image[0]; ?>" width="<?php echo $image[1]; ?>" height="<?php echo $image[2]; ?>" />
